93da410dc804510cb896ee69ed4c0eac1de8d79a
NeoZeRGG <d.romashov92@gmail.com>
2020-11-03 20:04:18 +0300

    added new files

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+const Header = () => {
+    return (
+      <header className='header'>
+      <img src='https://diskomir.ru/upload/resize_cache/webp/iblock/792/792542b2dbd1e1b41f6f32b9f3ad85a6.webp'></img>
+      </header>
+      );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import './App.css';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+    <div>
+      <img src='https://ixbt.online/gametech/covers/2020/10/25/vL19p9Bue61FjP2tOGsoW5YqErJftArY7jC4LbDQ.jpeg?w=760'></img>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+    </div>
+    <div>
+      New post
+    </div>
+    <div>
+      post 1
+    </div>
+    <div>
+      post 2
+    </div>
+  </div>
+  );
+}
+
+export default Profile;
